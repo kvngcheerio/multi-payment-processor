309ccf481ab62394effffc71acc6468ab27cb1c1
Omozue Gregory <trackcompton@gmail.com>
2022-05-22 20:55:45 +0100

    Initiate and Verify Payment Transactions (#8)
    * checkout functions
    
    * reduced amount on response

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ require('./utils');
 require('./processors')
 const {getAllBanks} = require('./requests/bankList');
 const {verifyBankAccount} = require('./requests/accountVerification')
+const {initiateCheckout, verifyTransaction} = require('./requests/checkout')
 
 
 module.exports = {
     getAllBanks,
     verifyBankAccount,
+    initiateCheckout,
+    verifyTransaction
 }
--- requests/index.js
+++ requests/index.js
@@ -1,6 +1,9 @@
-const getAllBanks = require('./bankList')
-const verifyBankAccount = require('./accountVerification')
+const {getAllBanks} = require('./bankList')
+const {verifyBankAccount} = require('./accountVerification')
+const {initiateCheckout, verifyTransaction} = require('./checkout')
 module.exports = {
     getAllBanks,
     verifyBankAccount,
+    initiateCheckout,
+    verifyTransaction
 }
