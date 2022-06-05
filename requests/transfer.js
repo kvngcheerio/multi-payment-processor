06243f2b8285d89af0f4536f826e5228d2aec050
Omozue Gregory <trackcompton@gmail.com>
2022-06-05 19:36:43 +0100

    flutterwave initiate transfer (#13)

---
+++ requests/transfer.js
@@ -0,0 +1,22 @@
+const {processor} = require('../config/environment');
+const {flutterwave} = require('../processors');
+
+const initiateTransfer = async({bankCode:bankCode, accountNumber:accountNumber, amount:amount, currency:currency, narration:narration, callbackUrl:callbackUrl, others}) => {
+    try{
+        if(!processor) 
+            throw 'Please enter PROCESSOR in env file';
+        switch (processor) {
+            case "FLUTTERWAVE":return flutterwave.flutterwaveInitiateCheckout(bankCode, accountNumber, amount, currency, narration, callbackUrl, others);
+          }
+    }
+    catch(e){
+        throw 'intiating transfer failed';
+    }
+}
+
+
+
+
+module.exports = {
+    initiateTransfer
+}
